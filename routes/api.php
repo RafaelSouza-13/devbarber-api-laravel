5069cdbaa573b44261599b68efbfeb66c0ab80f1
Rafael Souza <rafa.nds13@gmail.com>
2025-07-14 13:22:32 -0300

    Criando rotas e adiconando novos controladores

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,35 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserFavoriteController;
+use App\Http\Controllers\UserAppointmentController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BarberController;
+use App\Http\Controllers\UserFavorieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:api')->group(function () {
+
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+
+    Route::get('/user', [UserController::class, 'findAll']);
+    Route::get('/user/favorites', [UserFavoriteController::class, 'findAll']);
+    Route::post('/user/favorites', [UserFavoriteController::class, 'create']);
+
+    Route::get('/user/appointments', [UserAppointmentController::class, 'findAll']);
+
+    Route::get('/barbers', [BarberController::class, 'findAll']);
+    Route::get('/barber/{id}', [BarberController::class, 'findById']);
+
+    Route::post('/barber/{id}/appointment', [UserAppointmentController::class, 'create']);
+
+
+
+    Route::get('/search', [BarberController::class, 'search']);
+
+});
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/user', [UserController::class, 'create']);
