d6c6dc89e7ca689a51be8b6ce7a32500640d1f65
Rafael Souza <rafa.nds13@gmail.com>
2025-07-15 13:50:13 -0300

    Implementando a o método de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(LoginRequest $request){
+        $data = $request->validated();
+        $token = auth()->attempt([
+            'email' => $data['email'],
+            'password' => $data['password']
+        ]);
+
+        if(!$token){
+            return response()->json([
+                'success' => false,
+                'message' => "Credenciais inválidas",
+            ], 401);
+        }
+        $user = auth()->user();
+        $data['user']['name'] = $user->name;
+        $data['user']['email'] = $user->email;
+        $data['user']['avatar'] = url('media/avatars'.$user->avatar);
+        $data['token'] = $token;
+        return response()->json($data, 200);
+    }
 }
